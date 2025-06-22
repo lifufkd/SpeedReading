91dcc0ba32f246fa8c20a60b84f877faf26c9c6a
SBR <dmsemen45@gmail.com>
2025-06-22 22:31:28 +0300

    Make import link absolute

--- src/repositories/users.py
+++ src/repositories/users.py
@@ -1,7 +1,7 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
 
-from models import Users
+from src.models.users import Users
 from src.repositories.abstract.users import UserAbstract
 
 
