5efd1de77976b680bf02e95acd64ee3cee677ae1
zzsxd <86166681+zzsxd@users.noreply.github.com>
2025-06-24 01:40:15 +0800

    add fix middleware

--- src/main.py
+++ src/main.py
@@ -18,12 +18,12 @@ async def lifespan(_: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 
-middleware = CORSMiddleware(
-    app=app,
-    allow_origins=["*"],
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
     allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=["*"]
+    allow_headers=["*"],
 )
 
 app.include_router(api_v1_router, prefix="/api/v1")
