95fd02b1a76f938860fdd57a69bc4626917e346d
SBR <dmsemen45@gmail.com>
2025-06-27 19:44:13 +0300

    Add courses business logic

---
+++ src/repositories/abstract/courses.py
@@ -0,0 +1,31 @@
+from abc import ABC, abstractmethod
+
+from src.models.course import Courses
+from src.dto.courses import CreateCoursesDTO, UpdateCoursesDTO
+
+
+class CoursesAbstract(ABC):
+
+    @abstractmethod
+    async def get_all(self) -> list[Courses]:
+        pass
+
+    @abstractmethod
+    async def add(self, data: CreateCoursesDTO) -> Courses:
+        pass
+
+    @abstractmethod
+    async def get_by_id(self, course_id: int) -> Courses:
+        pass
+
+    @abstractmethod
+    async def get_by_ids(self, courses_ids: list[int]) -> list[Courses]:
+        pass
+
+    @abstractmethod
+    async def update(self, course_id: int, data: UpdateCoursesDTO) -> Courses | None:
+        pass
+
+    @abstractmethod
+    async def delete(self, course_id: int) -> bool | None:
+        pass
